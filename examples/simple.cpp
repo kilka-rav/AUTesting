2adb26d0ca0eed458bf21ed3a8e37842b67621dd
kilka-rav <zakirianov.rv@phystech.edu>
2024-01-22 00:40:36 +0300

    Fixed example

--- examples/simple.cpp
+++ examples/simple.cpp
@@ -13,14 +13,3 @@ template <typename T>
 T sum(T a, T b) {
     return a + b;
 }
-
-int main() {
-    int a = 1;
-    int b = 3;
-    int c;
-    sum(a, b, c);
-    sum(a, b);
-    float d = 1.0;
-    float e = 2.0;
-    sum(d, e);
-}
